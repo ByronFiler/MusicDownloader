e202c3408c002691c353f1977903f9a4cd00fca9
Byron Filer <byronfiler348@gmail.com>
2020-09-17 15:52:15 +0100

    Added support for Japanese

--- src/main/musicdownloader/Main.java
+++ src/main/musicdownloader/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 import musicdownloader.model.Model;
 import musicdownloader.utils.app.Debug;
 import musicdownloader.utils.ui.Notification;
@@ -22,21 +23,19 @@ import java.util.ResourceBundle;
 /*
 TODO
  Support different search databases: https://en.wikipedia.org/wiki/List_of_online_music_databases
+ Allow users to select their own language overwriting local locale *heh*
  */
 
 public class Main extends Application {
 
-    // TODO: Move this somewhere else
     static {
-        Locale.setDefault(new Locale("ru"));
-
+        Locale.setDefault(new Locale("cn"));
         try {
             ResourceBundle.getBundle("resources.locale.search");
         } catch (MissingResourceException ignored) {
             Debug.warn("Users current locale " + Locale.getDefault() + " is not supported, defaulting to English.");
             Locale.setDefault(new Locale("en", "GB"));
         }
-
     }
 
     @Override
@@ -50,33 +49,9 @@ public class Main extends Application {
 
         primaryStage.setScene(new Scene(root));
         primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("resources/img/icon.png"))));
-        primaryStage.setTitle("Music Downloader");
+        primaryStage.setTitle(ResourceBundle.getBundle("resources.locale.search").getString("hero"));
         primaryStage.show();
-
-        primaryStage.setOnCloseRequest(e -> {
-            Model.getInstance().setStageClosed(true);
-            Debug.trace("Primary stage closed.");
-            if (!Model.getInstance().download.getDownloadObject().toString().equals(new JSONObject().toString())) {
-
-                try {
-
-                    int incompleteSongs = 0;
-                    for (int i = 0; i < Model.getInstance().download.getDownloadObject().getJSONArray("songs").length(); i++) {
-                        if (!Model.getInstance().download.getDownloadObject().getJSONArray("songs").getJSONObject(i).getBoolean("completed")) incompleteSongs++;
-                    }
-
-                    new Notification(
-                            String.format("Downloading \"%s\" in Background...", Model.getInstance().download.getDownloadObject().getJSONObject("metadata").getString("album")),
-                            String.format("%s song%s remaining.", incompleteSongs, incompleteSongs == 1 ? "" : "s"),
-                            null,
-                            TrayIcon.MessageType.INFO
-                    );
-                } catch (JSONException er) {
-                    Debug.warn("Failed to send downloads notification on window close, review download object.");
-                }
-
-            }
-        });
+        primaryStage.setOnCloseRequest(this::handleClose);
 
         Model.getInstance().setPrimaryStage(primaryStage);
 
@@ -86,5 +61,30 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private void handleClose(WindowEvent event) {
+        Model.getInstance().setStageClosed(true);
+        Debug.trace("Primary stage closed.");
+        if (!Model.getInstance().download.getDownloadObject().toString().equals(new JSONObject().toString())) {
+
+            try {
+
+                int incompleteSongs = 0;
+                for (int i = 0; i < Model.getInstance().download.getDownloadObject().getJSONArray("songs").length(); i++) {
+                    if (!Model.getInstance().download.getDownloadObject().getJSONArray("songs").getJSONObject(i).getBoolean("completed")) incompleteSongs++;
+                }
+
+                new Notification(
+                        String.format("Downloading \"%s\" in Background...", Model.getInstance().download.getDownloadObject().getJSONObject("metadata").getString("album")),
+                        String.format("%s song%s remaining.", incompleteSongs, incompleteSongs == 1 ? "" : "s"),
+                        null,
+                        TrayIcon.MessageType.INFO
+                );
+            } catch (JSONException er) {
+                Debug.warn("Failed to send downloads notification on window close, review download object.");
+            }
+
+        }
     }
 }
